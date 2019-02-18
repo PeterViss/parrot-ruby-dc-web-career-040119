03d38e3393d4fcea5debb6e81fb3e3c9141e3546
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-18 03:51:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,8 @@
-def parrot(phrase = "Squawk!", bird = "Pretty bird!")
+def parrot(phrase = "Squawk!")
    return "#{phrase}"
-   return "#{bird}"
 end  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase)
+  return ("Pretty bird!")
+end
